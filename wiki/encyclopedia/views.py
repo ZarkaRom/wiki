0f8c162dd54cb23d8cf123fe5045b29e347e0933
zarkarom <romaldo.zarka+github@gmail.com>
2024-11-18 22:34:02 +0100

    I added function to get the link

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 
 from . import util
-
+from django.http import HttpResponse 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def page_view(request, title):
+    return HttpResponse (f'the title is: {title}')
